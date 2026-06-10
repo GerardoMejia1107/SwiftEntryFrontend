de1caf17d2250388d42278ea262beaa9a0253403
Herberth Alexander Ventura Chavez <00116223@uca.edu.sv>
2026-06-09 19:28:13 -0600

    added the testing components for roles tests login

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import LoginPage from './pages/login/LoginPage';
 import RegisterOrganizerPage from './pages/registerOrganizer/RegisterOrganizerPage';
 import RegisterPage from './pages/registerUser/RegisterPage';
 
-export default function App() {
-  const [page, setPage] = useState('register'); // 'login', 'register', 'register-organizer'
+const WelcomeUser = () => <h1>Bienvenido usuario normal</h1>;
+const WelcomeOrganizer = () => <h1>Bienvenido organizador</h1>;
+const WelcomeAdmin = () => <h1>Bienvenido usuario administrador</h1>;
 
+export default function App() {
   return (
-    <div className="app">
-      {page === 'login' && <LoginPage onNavigate={setPage} />}
-      {page === 'register' && <RegisterPage onNavigate={setPage} />}
-      {page === 'register-organizer' && <RegisterOrganizerPage onNavigate={setPage} />}
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/register-organizer" element={<RegisterOrganizerPage />} />
+          
+          
+          <Route path="/home-user" element={<WelcomeUser />} />
+          <Route path="/home-organizer" element={<WelcomeOrganizer />} />
+          <Route path="/home-admin" element={<WelcomeAdmin />} />
+          
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
